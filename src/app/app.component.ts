75c411959181c67286c53abbdfdbd3c103c68cb8
vinay kumar maurya <v7naym@gmail.com>
2020-11-19 18:56:34 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import{webSocket,WebSocketSubject} from 'rxjs/webSocket';
+import{filter, reduce} from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'liveStock';
-}
+  subject = webSocket('ws://stocks.hulqmedia.com');
+  stocks:any;
+
+ 
+  
+  ngOnInit():void{
+    this.subject.subscribe(x=>{
+      //this.stocks = x[0];
+      this.stocks = x;
+      
+      //console.log('x----------------------',x);
+      console.log('------------y',this.stocks);
+      
+      
+    })
+    // distinct value
+    // this.subject.pipe(distinctUntilChanged()).subscribe(x=>{
+    //   this.stocks = x;
+    //   console.log('--------------->',this.stocks);
+    // })
+
+
+
+
+
+    // this.subject.startWith(null) // emitting first empty value to fill-in the buffer
+    // .pairwise()
+    // .subscribe([previousValue, currentValue] => {
+    //     if (null === previousValue) {
+    //         console.log('Probably first emission...');
+    //     }
+    // })
+   
+  }
+  // getColor(stock){
+  //   switch(stock){
+  //     case 'stock<60':
+  //       return 'red';
+  //     case 'stock>60':
+  //       return 'green';
+  //   }
+
+  }
+  
+
+
+  
+
