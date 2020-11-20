044e189634bbbf6a46da3c55133d39aa37bd1bbb
vinay kumar maurya <v7naym@gmail.com>
2020-11-20 17:02:06 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,61 @@
-import { Component,OnInit} from '@angular/core';
-import{webSocket,WebSocketSubject} from 'rxjs/webSocket';
-import{filter, reduce} from 'rxjs/operators';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { filter, reduce } from 'rxjs/operators';
 import { distinctUntilChanged } from 'rxjs/operators';
 
-
+export interface IStockPrice {
+  value: number;
+  change?: 'UP' | 'DOWN' | 'NONE';
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'liveStock';
   subject = webSocket('ws://stocks.hulqmedia.com');
-  stocks:any;
+  stocks: Map<string, IStockPrice>;
 
- 
-  
-  ngOnInit():void{
-    this.subject.subscribe(x=>{
+  constructor(private cd: ChangeDetectorRef) {
+    this.stocks = new Map();
+  }
+
+  ngOnInit(): void {
+    this.subject.subscribe((x: [any]) => {
       //this.stocks = x[0];
-      this.stocks = x;
-      
-      //console.log('x----------------------',x);
-      console.log('------------y',this.stocks);
-      
-      
-    })
+      // this.stocks = x;
+      for (const stock of x) {
+        const key = stock[0];
+        const value = stock[1];
+        if (this.stocks.has(key)) {
+          const oldValue = this.stocks.get(key).value;
+          const vchange = value - oldValue;
+          let change: 'UP' | 'DOWN' | 'NONE';
+          if (vchange < 0) {
+            change = 'DOWN';
+          } else if (vchange > 0) {
+            change = 'UP';
+          } else {
+            change = 'NONE';
+          }
+          this.stocks.set(key, { value: value, change: change });
+          this.cd.detectChanges();
+        } else {
+          this.stocks.set(key, { value: value, change: 'NONE' });
+          this.cd.detectChanges();
+        }
+      }
+      // console.log('x----------------------', x);
+      // console.log('------------y', this.stocks);
+    });
     // distinct value
     // this.subject.pipe(distinctUntilChanged()).subscribe(x=>{
     //   this.stocks = x;
     //   console.log('--------------->',this.stocks);
     // })
-
-
-
-
 
     // this.subject.startWith(null) // emitting first empty value to fill-in the buffer
     // .pairwise()
@@ -44,7 +64,6 @@ export class AppComponent implements OnInit{
     //         console.log('Probably first emission...');
     //     }
     // })
-   
   }
   // getColor(stock){
   //   switch(stock){
@@ -53,10 +72,4 @@ export class AppComponent implements OnInit{
   //     case 'stock>60':
   //       return 'green';
   //   }
-
-  }
-  
-
-
-  
-
+}
